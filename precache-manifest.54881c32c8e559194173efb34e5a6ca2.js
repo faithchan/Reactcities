c3ba5abae19bbace0b04e3e5e2725032a59f9d11
faithchan <fch@mercantilemaritime.com>
2021-10-12 15:04:46 +0800

    Updates

---
+++ precache-manifest.54881c32c8e559194173efb34e5a6ca2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd2e965d17f6cb18ec7a3d972a72a898",
+    "url": "/Reactcities/index.html"
+  },
+  {
+    "revision": "371fbb547cdba66cc10a",
+    "url": "/Reactcities/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "3c2968ef8b2eb466d66b",
+    "url": "/Reactcities/static/js/2.32bdae52.chunk.js"
+  },
+  {
+    "revision": "371fbb547cdba66cc10a",
+    "url": "/Reactcities/static/js/main.da8c351f.chunk.js"
+  },
+  {
+    "revision": "44d2dc5ad38f0aacb864",
+    "url": "/Reactcities/static/js/runtime~main.ac27e5ca.js"
+  }
+]);
